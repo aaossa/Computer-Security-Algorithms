a818e4eee91a0a0bdc695a989b7a96502d920905
aaossa <aaossa@uc.cl>
2015-12-20 18:14:44 -0300

    Intro to RSA complete

--- 21 - Intro to RSA/intro-to-RSA.py
+++ 21 - Intro to RSA/intro-to-RSA.py
@@ -67,9 +67,9 @@ class RSA:
 
 def demo():
     public_key = (
-        0x3, 0x64ac4671cb4401e906cd273a2ecbc679f55b879f0ecb25eefcb377ac724ee3b1)
-    private_key = 0x431d844bdcd801460488c4d17487d9a5ccc95698301d6ab2e218e4b575d52ea3
-    c = 0x599f55a1b0520a19233c169b8c339f10695f9e61c92bd8fd3c17c8bba0d5677e
+        int('3', 16), int('64ac4671cb4401e906cd273a2ecbc679f55b879f0ecb25eefcb377ac724ee3b1', 16))
+    private_key = int('431d844bdcd801460488c4d17487d9a5ccc95698301d6ab2e218e4b575d52ea3', 16)
+    c = int('599f55a1b0520a19233c169b8c339f10695f9e61c92bd8fd3c17c8bba0d5677e', 16)
 
     return RSA.Decryption(c, public_key, private_key)
 
@@ -85,3 +85,5 @@ if __name__ == '__main__':
 
     m = RSA.Decryption(c, public_key, private_key)
     print('c = {} decrypted is {}'.format(c, m))
+
+    print(hex(demo()))
