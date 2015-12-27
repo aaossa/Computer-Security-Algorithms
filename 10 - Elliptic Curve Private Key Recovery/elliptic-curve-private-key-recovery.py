9ff325c2599e56042217412f3ab6e252e0403fb9
aaossa <aaossa@uc.cl>
2015-12-27 13:23:32 -0300

    Here we go again...

--- 10 - Elliptic Curve Private Key Recovery/elliptic-curve-private-key-recovery.py
+++ 10 - Elliptic Curve Private Key Recovery/elliptic-curve-private-key-recovery.py
@@ -1,3 +1,5 @@
+from EllipticCurve import ECPoint, EllipticCurve
+
 # Elliptic Curve Private Key Recovery
 # https://id0-rsa.pub/problem/10/
 
@@ -9,35 +11,23 @@
 # http://stackoverflow.com/questions/31074172/
 
 
-class Elliptic_Curve():
-
-    @property
-    def G(self):
-        return (self.G_0, self.G_1)
-
-    @property
-    def Q(self):
-        return (self.Q_0, self.Q_1)
-
-
 if __name__ == '__main__':
-    A = Elliptic_Curve()
-    A.p = int(
-        '0xffffffff00000001000000000000000000000000ffffffffffffffffffffffff', 16)
     # ELliptic curve parameters
-    A.a = int(
+    p = int(
+        '0xffffffff00000001000000000000000000000000ffffffffffffffffffffffff', 16)
+    a = int(
         '-0x3', 16)
-    A.b = int(
+    b = int(
         '0x5ac635d8aa3a93e7b3ebbd55769886bc651d06b0cc53b0f63bce3c3e27d2604b', 16)
     # Base point
-    A.G_0 = int(
+    G_0 = int(
         '0x6b17d1f2e12c4247f8bce6e563a440f277037d812deb33a0f4a13945d898c296', 16)
-    A.G_1 = int(
+    G_1 = int(
         '0x4fe342e2fe1a7f9b8ee7eb4a7c0f9e162bce33576b315ececbb6406837bf51f5', 16)
     # Public key
-    A.Q_0 = int(
+    Q_0 = int(
         '0x52910a011565810be90d03a299cb55851bab33236b7459b21db82b9f5c1874fe', 16)
-    A.Q_1 = int(
+    Q_1 = int(
         '0xe3d03339f660528d511c2b1865bcdfd105490ffc4c597233dd2b2504ca42a562', 16)
 
     # Steps:
@@ -46,6 +36,19 @@ if __name__ == '__main__':
     # yields the point Q. Finding d given Q and G reduces to the discrete log
     # problem. Luckily, in this problem d was chosen to be a small value.
     # G * d = Q
+    EC = EllipticCurve(a, b, p=p, base_point=(G_0, G_1))
+    Q = ECPoint(Q_0, Q_1, EC)
+    G = EC.base_point
+    x = 0
+    d = 6015000
+    while not G.x == Q.x:
+        G = d * G
+        if not d % 5000:
+            print(d)
+        d += 1
+        # 5640000
+    print(G)
+    print(d)
 
     # Given the elliptic curve parameters (NIST curve P-256) and public key
     # find the value of the private key
